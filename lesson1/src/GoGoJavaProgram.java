ee493ec904cbe832e0a38f31739c00cf5fc602da
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:30:01 +0300

    Second lecture

--- lesson1/src/GoGoJavaProgram.java
+++
@@ -1,9 +0,0 @@
-public class GoGoJavaProgram {
-
-    public static void main(String[] args) {
-        System.out.println("Go GO!");
-        System.out.println("Java World!");
-        System.out.println("We are coming!");
-    }
-
-}
