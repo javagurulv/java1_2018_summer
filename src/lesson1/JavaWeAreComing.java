b1a598ef54794868b2fc25861201ec13a27897e8
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:34:00 +0300

    First lecture

---
+++ src/lesson1/JavaWeAreComing.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class JavaWeAreComing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Java We are coming!");
+    }
+
+}
