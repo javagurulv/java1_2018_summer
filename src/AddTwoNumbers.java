2f6a2deb25cb7d2025af4f11d97d53de9b89ccba
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:30:23 +0300

    Second lecture

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Get first number from user
+        System.out.println("Enter first number:");
+        int firstNumber = sc.nextInt();
+
+        // Get second number from user
+        System.out.println("Enter second number:");
+        int secondNumber = sc.nextInt();
+
+        // Add two numbers
+        int sum = firstNumber + secondNumber;
+
+        // Print result to console
+        System.out.println("Sum = " + sum);
+
+
+
+
+    }
+
+}
---
+++ src/Experement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Experement {
+
+    public static int getNumberFromUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        return sc.nextInt();
+    }
+
+    public static int findMaxNumber(int firstNumber, int secondNumber) {
+        if (firstNumber > secondNumber) {
+            return firstNumber;
+        } else {
+            return secondNumber;
+        }
+    }
+
+    public static void printMaxNumberToConsole(int maxNumber) {
+        System.out.println("Greatest number = " + maxNumber);
+    }
+
+    public static void main(String[] args) {
+        int firstNumber = getNumberFromUser();
+        int secondNumber = getNumberFromUser();
+        int maxNumber = findMaxNumber(firstNumber, secondNumber);
+        printMaxNumberToConsole(maxNumber);
+    }
+
+}
