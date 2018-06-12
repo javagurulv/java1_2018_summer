77bfb22e3fa59cb991a261371186431d528ae9de
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:50:12 +0300

    VariableNameExample

---
+++ src/lesson2/VariableNameExample.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+public class VariableNameExample {
+
+    public static void main(String[] args) {
+
+        // variable name start with small letter
+        int shipCount = 10;
+
+        // can be one, two, three words, each word start with big letter
+        double windowLengthInMeters = 1.1;
+
+        // boolean variable type, can store only true/false
+        boolean isMarried = true;
+        boolean haveChild = false;
+
+        // this is just a string (text)
+        String bookTitle = "Clean Code";
+    }
+
+}
