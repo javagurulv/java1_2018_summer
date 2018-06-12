2765d3c7a6f34232f517bf2684b9338181708b86
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:36:29 +0300

    CommentsExample

---
+++ src/lesson2/CommentsExample.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class CommentsExample {
+
+    public static void main(String[] args) {
+        // this is a comment till end of line
+
+        // write here any text or algorithm
+
+        // you can use any human words in your comments
+
+        // compiler just ignore comment lines when he compiles the code
+
+    }
+
+}
