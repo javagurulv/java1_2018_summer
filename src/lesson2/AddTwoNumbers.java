b9ec5ecc0d5db3186fb6261e524e0cda7aadeefc
Viktor Savonin <viktor.savonin@meawallet.com>
2018-06-12 23:41:39 +0300

    AddTwoNumbers code and comments

--- src/lesson2/AddTwoNumbers.java
+++ src/lesson2/AddTwoNumbers.java
@@ -5,26 +5,22 @@ import java.util.Scanner;
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
-
+        // Create Scanner object to read data from console
         Scanner sc = new Scanner(System.in);
 
-        // Get first number from user
+        // Get first number from user and store it to variable named firstNumber
         System.out.println("Enter first number:");
         int firstNumber = sc.nextInt();
 
-        // Get second number from user
+        // Get second number from user  and store it to variable named secondNumber
         System.out.println("Enter second number:");
         int secondNumber = sc.nextInt();
 
-        // Add two numbers
+        // Add two numbers and store result to variable named sum
         int sum = firstNumber + secondNumber;
 
-        // Print result to console
+        // Print variable sum (its current value) to console
         System.out.println("Sum = " + sum);
-
-
-
-
     }
 
 }
