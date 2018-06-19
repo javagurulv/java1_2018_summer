e37bc495586ffedec8a68b3cc4492f506f3458d0
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-06-19 20:10:11 +0300

    doWhile example

---
+++ src/lesson3/DoWhileExample.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+class DoWhileExample {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println(counter);
+        } while (counter++ < 100);
+    }
+}
