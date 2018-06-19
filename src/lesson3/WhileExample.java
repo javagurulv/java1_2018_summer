8424670e273c176c1152d68385c0e27ccfe706bf
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-06-19 20:04:21 +0300

    while example

---
+++ src/lesson3/WhileExample.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+class WhileExample {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 101) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
